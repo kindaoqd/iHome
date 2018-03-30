ce9b33d3dc8c3a1f10353e6f02b058cf1e70df39
kinder <1048994031@qq.com>
2018-03-30 15:07:15 +0800

    app config & create db

--- manager.py
+++ manager.py
@@ -1,7 +1,23 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """应用配置类"""
+    # 调试设置
+    DEBUG = True
+    # 数据库关联配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 密钥设置
+    SECRET_KEY = 'mgWj5NFlRvf8h2hGDJhSvzrR9VsxKfAF4Z6KorsK6gDOzhOAIstWcJEA9+JHj2CW'
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
